be321e961a5392c2eb8fcb661e6c96f5754a5c61
Febriyadi <flutterlearning6@gmail.com>
2024-04-15 18:31:51 +0800

    add data type number

---
+++ dart_fundamental/bin/Tipe Data/number.dart
@@ -0,0 +1,10 @@
+void main() {
+  // num -> int and double
+  double pi = -9 * 22 / 7;
+  print(pi);
+  print("-----");
+  print(pi.ceil()); //ceil int. fungsi ceil membulatkan angka diatasnya
+  print("-----");
+  print(pi.ceilToDouble()); //ceil double
+  print(pi.abs()); // membuat daa selalu positif /Absolut
+}
