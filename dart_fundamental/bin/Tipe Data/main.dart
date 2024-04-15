93d4d38992f7affcf4d3b6686fec7ec69818fe95
Febriyadi <flutterlearning6@gmail.com>
2024-04-15 18:04:09 +0800

    add data type map and list topic

--- dart_fundamental/bin/Tipe Data/main.dart
+++ dart_fundamental/bin/Tipe Data/main.dart
@@ -1,9 +1,23 @@
-import 'dart:io';
+// import 'dart:io';
 
 void main() {
-  stdout.write('Nama Anda : ');
-  String name = stdin.readLineSync()!;
-  stdout.write('Usia Anda : ');
-  int age = int.parse(stdin.readLineSync()!);
-  print('Halo $name, usia Anda $age tahun');
+  // stdout.write('Nama Anda : ');
+  // String name = stdin.readLineSync()!;
+  // stdout.write('Usia Anda : ');
+  // int age = int.parse(stdin.readLineSync()!);
+  // print('Halo $name, usia Anda $age tahun');
+
+  /*
+  List pada Dart
+  */
+  List warna = ["Coklat", "Merah", "Biru"];
+  print(warna);
+  /*
+  Mapping pada Dart
+  */
+  Map<String, dynamic> user = {
+    "name": "Febriyadi",
+    "age": 99,
+  };
+  print(user["name"] + "Umur :" + user["age"].toString());
 }
