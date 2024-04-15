ae7ac30327ca918d4a1326c668c5c32293358cb0
Febriyadi <flutterlearning6@gmail.com>
2024-04-15 17:45:08 +0800

    add data type topic

---
+++ dart_fundamental/bin/Tipe Data/main.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Nama Anda : ');
+  String name = stdin.readLineSync()!;
+  stdout.write('Usia Anda : ');
+  int age = int.parse(stdin.readLineSync()!);
+  print('Halo $name, usia Anda $age tahun');
+}
