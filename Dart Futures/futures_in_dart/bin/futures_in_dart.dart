48398d2b340164cff8d932d263f21014cfb50629
Febriyadi <flutterlearning6@gmail.com>
2024-04-17 06:18:24 +0800

    Future with async-await topic

---
+++ Dart Futures/futures_in_dart/bin/futures_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:futures_in_dart/futures_in_dart.dart' as futures_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${futures_in_dart.calculate()}!');
+}
---
+++ Dart Futures/futures_in_dart/test/futures_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:futures_in_dart/futures_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
