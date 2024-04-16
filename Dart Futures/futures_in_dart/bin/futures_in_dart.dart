ace9caf941cc0417612e628b063e88125230c9aa
Febriyadi <flutterlearning6@gmail.com>
2024-04-17 07:11:55 +0800

    add asynchronous in dart topic

--- Dart Futures/futures_in_dart/bin/futures_in_dart.dart
+++ Dart Futures/futures_in_dart/bin/futures_in_dart.dart
@@ -1,5 +1,54 @@
-import 'package:futures_in_dart/futures_in_dart.dart' as futures_in_dart;
+// 3 jenis status dalam asynchronous
+//1 . Uncompleted -> blm sukses / masih proses
+// 2. Completed -> data (Sukses)
+// 3. Completed -> error sukses tapi(error)
+void main() async {
+  //cara asynchronous biasa
+  // getOrder(1).then((String status) {//Menangkap datanya
+  //   print("dijalankan");
+  //   print(status);
+  // }).catchError((error) {
+  //   print(error);
+  // });
 
-void main(List<String> arguments) {
-  print('Hello world: ${futures_in_dart.calculate()}!');
+  //membuat async-> layaknya proses sync (biasa)
+  // await getOrder(1).then((String status) {
+  //   //Menangkap datanya
+  //   print("dijalankan");
+  //   print(status);
+  // }).catchError((error) {
+  //   print(error);
+  // });
+
+  //lebih singkat mennggunakan try catch
+  try {
+    String status = await getOrder(10);
+  } catch (error) {
+    print("yang ini dijalankan");
+    print(error);
+  } finally {
+    pertama();
+    kedua();
+  }
+  pertama();
+  kedua();
+}
+
+void pertama() {
+  print("data 1");
+}
+
+void kedua() {
+  print("data 2");
+}
+
+Future<String> getOrder(int beli) {
+  int stok = 5;
+  return Future.delayed(Duration(seconds: 1), () {
+    if (stok > beli) {
+      return "Berhasil membeli barang sebanyak $beli";
+    } else {
+      throw "Tidak berhasil membeli barang kerena stok kurang"; //ubah throw agar tidak dibaca oleh then
+    }
+  });
 }
