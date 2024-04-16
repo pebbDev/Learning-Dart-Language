9a0ac712cbd66fd3da4482e342bab588c9c4bb77
Febriyadi <flutterlearning6@gmail.com>
2024-04-16 11:31:00 +0800

    add looping topic

---
+++ dart_fundamental/bin/Perulangan/for.dart
@@ -0,0 +1,6 @@
+void main() {
+  for (int count = 1; count <= 10; count++) {
+    print(count);
+  }
+  ;
+}
---
+++ dart_fundamental/bin/Perulangan/while.dart
@@ -0,0 +1,21 @@
+void main() {
+  int nilaiAwal = 10;
+  int nilaiAkhir = 100;
+  while (nilaiAkhir <= nilaiAkhir) {
+    print(nilaiAwal);
+    nilaiAkhir++;
+
+    //do while
+    int count = 0;
+    do {
+      print(count);
+      count++;
+    } while (count <= 10);
+  }
+
+  //Infiniti loop
+  int count = 1;
+  do {
+    print(count);
+  } while (count <= 10);
+}
