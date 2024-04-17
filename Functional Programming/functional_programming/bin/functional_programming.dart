5cc4c7fc8392e6147a9ed60fcc0a3ffd183218fc
Febriyadi <flutterlearning6@gmail.com>
2024-04-17 11:45:33 +0800

    this anonymous function

--- Functional Programming/functional_programming/bin/functional_programming.dart
+++
@@ -1,5 +0,0 @@
-import 'package:functional_programming/functional_programming.dart' as functional_programming;
-
-void main(List<String> arguments) {
-  print('Hello world: ${functional_programming.calculate()}!');
-}
