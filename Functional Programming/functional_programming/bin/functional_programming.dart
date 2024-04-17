813f49333ef27dbb1355bb3d62d23a9b976bf111
Febriyadi <flutterlearning6@gmail.com>
2024-04-17 11:35:42 +0800

    first commit functional_programming topic

---
+++ Functional Programming/functional_programming/bin/functional_programming.dart
@@ -0,0 +1,5 @@
+import 'package:functional_programming/functional_programming.dart' as functional_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${functional_programming.calculate()}!');
+}
